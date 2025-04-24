23efdb4b40bed00238b96c24705a5ee4ab5783da
Dawken <dawid.dominski1125@gmail.com>
2025-04-24 11:51:53 +0200

    feat: create basic configuration dash video player, display/choose quality

--- src/features/Watch/VideoPlayer.vue
+++ src/features/Watch/VideoPlayer.vue
@@ -1,4 +1,103 @@
 <script setup lang="ts">
+import { onMounted, ref, watch, onUnmounted } from "vue";
+import { MediaPlayer, type Bitrate, type MediaPlayerClass } from "dashjs";
+
+const videoRef = ref<HTMLVideoElement | null>(null);
+const player = ref<MediaPlayerClass | null>(null);
+const bitrates = ref<Bitrate[]>([]);
+const selectedQuality = ref<number>(0);
+
+const { src } = defineProps<{ src: string }>();
+
+const initializePlayer = () => {
+  if (!videoRef.value) return;
+
+  player.value = MediaPlayer().create();
+  player.value.initialize(videoRef.value, src, true);
+
+  player.value.updateSettings({
+    streaming: {
+      abr: {
+        autoSwitchBitrate: {
+          video: false,
+        },
+      },
+    },
+  });
+
+  player.value.on(MediaPlayer.events.STREAM_INITIALIZED, () => {
+    const bitrateList =
+      player.value?.getTracksFor("video")[0].bitrateList || [];
+    bitrates.value = bitrateList;
+
+    if (!player.value) return;
+
+    const availableQualities = player.value.getRepresentationsByType("video");
+    const targetRepresentationIndex = availableQualities.length - 1;
+    player.value?.setRepresentationForTypeByIndex(
+      "video",
+      targetRepresentationIndex
+    );
+  });
+};
+
+const changeQuality = (index: number) => {
+  if (!player.value) {
+    return;
+  }
+
+  player.value.setRepresentationForTypeByIndex("video", index);
+
+  player.value.reset();
+
+  player.value.initialize(videoRef.value!, src, true);
+};
+
+watch(selectedQuality, (newIndex) => {
+  changeQuality(newIndex);
+});
+
+onMounted(() => {
+  initializePlayer();
+});
+
+onUnmounted(() => {
+  player.value?.reset();
+});
+</script>
+
+<template>
+  <div>
+    <video
+      ref="videoRef"
+      controls
+      width="1280"
+      height="720"
+      muted
+      playsinline
+    />
+
+    <div class="quality-selector" v-if="bitrates.length > 0">
+      <label for="quality-select">Quality:</label>
+      <select id="quality-select" v-model.number="selectedQuality">
+        <option
+          v-for="(track, index) in bitrates"
+          :key="track.id"
+          :value="index"
+        >
+          {{ track.height }}p
+          <div v-if="track.bandwidth">
+            ({{ Math.round(track.bandwidth / 1000) }} kbps)
+          </div>
+        </option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<!-- HLS -->
+
+<!-- <script setup lang="ts">
 import { ref, onMounted, onBeforeUnmount } from "vue";
 import Hls, { Level } from "hls.js";
 
@@ -52,8 +151,8 @@ const changeResolution = (event: Event) => {
       controls
       width="1280"
       height="720"
-      style="background: black"
-    ></video>
+      muted
+    />
 
     <div v-if="levels.length" style="margin-top: 10px">
       <label for="res-select">Rozdzielczość:</label>
@@ -69,4 +168,4 @@ const changeResolution = (event: Event) => {
       </select>
     </div>
   </div>
-</template>
+</template> -->
--- src/pages/Watch/Watch.vue
+++ src/pages/Watch/Watch.vue
@@ -2,9 +2,10 @@
 import VideoPlayer from "../../features/Watch/VideoPlayer.vue";
 </script>
 
+<!-- Dash: https://dash.akamaized.net/envivio/EnvivioDash3/manifest.mpd  -->
+<!-- HLS:  https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8 -->
 <template>
   <VideoPlayer
-    src="https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
-    type="video/mp4"
+    src="https://dash.akamaized.net/envivio/EnvivioDash3/manifest.mpd"
   />
 </template>
